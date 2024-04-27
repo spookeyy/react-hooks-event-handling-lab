22e580e266d51c8b888def5e5d84d37a535d4dae
spookeyy <pangasmeshack@gmail.com>
2024-04-27 13:57:45 +0300

    understood

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
 // Code Keypad Component Here
 
+// function handleChange(e){
+//     console.log(`${e.target.name}: ${e.target.value}`);
+// }
+
 function Keypad (){
     return (
-        <div></div>
-    )
+      <div>
+        <form>
+          <input
+            type="password"
+            name="password"
+            onChange={() => console.log("Entering password...")}
+            // onChange={()=> handleChange()}
+          ></input>
+        </form>
+      </div>
+    );
 }
 
 export default Keypad;
